dd2413d84910a65ae558e94df72c0a3ab63219eb
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-02-15 11:18:06 +0530

    Basics of React.js

---
+++ Video3.js
@@ -0,0 +1,26 @@
+/*
+------------------------- TOPICS ----------------------------------
+
+1. DOM Elements : HTML elements are dom elements.
+    - DOM elements are the individual components of a web page, such as paragraphs, headings, images, buttons, forms, etc.
+
+2. React.createElement : this will create a React element which is an object .
+    - when we render this using ReactDOM, it becomes React components or DOM elements, not HTML elements
+
+3. For displaying on to browser we use "ReactDOM".
+    - import ReactDOM from "react-dom/client" or we can write
+    - import ReactDOM from "react-dom"
+    - both above imports works
+    ! - React element is basically a object.
+
+4. JSX : It is not part of React.
+    - we can create a React app without jsx
+    - It is not html inside javascript.Lot of dev thinks it html inside javascript
+    - it's just syntax that looks like html but it's not actually html
+    - jsx is different than html
+    - jsx is different and React is different
+    - JSX is a syntax extension for JavaScript that allows us to write HTML-like code within JavaScript. However, JSX is not directly understood by web browsers. Instead, JSX code is transformed into standard JavaScript code by tools like Babel during the build process. This transformed JavaScript code is what actually gets executed by the browser. Therefore, JSX is just a developer-friendly syntax that simplifies writing React components, but it's ultimately converted into JavaScript code that the browser can understand.
+
+
+
+*/
