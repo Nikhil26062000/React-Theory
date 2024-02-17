450cb5e87f092df36dbca101b0b812c0d4d73dd1
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-02-17 13:00:05 +0530

    Higher order func() and lifting up the state and props drillling and context api

---
+++ Video11.js
@@ -0,0 +1,19 @@
+
+
+/*
+
+1. Higher order component:
+        - A higher order component is a function which takes a function as its argument and returns a function
+        -It is normal js function.
+
+2. Lifting up the state:
+        - controlled vs uncontrolled component
+
+3. Props drilling:
+        - passing props from parent component to child component
+        -to avoid props drilling we use React context.
+
+4. React Context:
+        - see video at -40:00 (not for theory , coding purpose)
+
+*/
