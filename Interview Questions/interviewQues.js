f275ea274f81e2049be307e8b42e1963946513f2
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-02-26 23:45:32 +0530

    Interview questions

---
+++ Interview Questions/interviewQues.js
@@ -0,0 +1,11 @@
+
+
+//? ----------- What are the features of React --------------------
+    //1. jsx
+    //2. components
+    //3. virtual dom : React keeps a lightweight representation of the real DOM in the memory, and that is known as the virtual DOM. When the state of an object changes, virtual DOM changes only that object in the real DOM, rather than updating all the objects.
+    //4. one-way data binding : React keeps a lightweight representation of the real DOM in the memory, and that is known as the virtual DOM. When the state of an object changes, virtual DOM changes only that object in the real DOM, rather than updating all the objects.
+    //5. High Performance :  React updates only those components that have changed, rather than updating all the components at once. This results in much faster web applications.
+
+//? ----------------------- What is the virtual DOM ----------------------
+    //React keeps a lightweight representation of the real DOM in the memory, and that is known as the virtual DOM. When the state of an object changes, the virtual DOM changes only that object in the real DOM, rather than updating all the objects
